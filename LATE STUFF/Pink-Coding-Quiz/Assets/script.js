1822597caf4a35863e621f06fca57f0789c3a448
Sam Moore <sammooresemail23@gmail.com>
2022-07-10 16:56:25 -0700

    initial commit

---
+++ LATE STUFF/Pink-Coding-Quiz/Assets/script.js
@@ -0,0 +1,17 @@
+//variable declarations
+var choiceA = ;
+var choiceB = ;
+var choiceC = ;
+var choiceD = ;
+var q1 = {};
+var q2 = {};
+var q3 = {};
+var q4 = {};
+var quizQuestions = [q1, q2, q3, q4,];
+var answerChoices = [A, B, C, D];
+
+
+
+
+// if statements to animate the quiz questions
+if ()
